b0e90643b2b874aa184d7b592f30f783f8c45e5c
Sean Bourg <sean.bourg@gmail.com>
2022-03-16 14:17:03 -0500

    changed to return if secondary monitor not present

--- src/modScaleThumbnails.js
+++ src/modScaleThumbnails.js
@@ -1,26 +1,26 @@
-const mod = imports.misc.extensionUtils.getCurrentExtension().imports.src.mod
+var mod = imports.misc.extensionUtils.getCurrentExtension().imports.src.mod
 
-const workspaceThumbnail = imports.ui.workspaceThumbnail
-const SecondaryMonitorDisplay = imports.ui.workspacesView.SecondaryMonitorDisplay
+var workspaceThumbnail = imports.ui.workspaceThumbnail
+var SecondaryMonitorDisplay = imports.ui.workspacesView.SecondaryMonitorDisplay
 
 var Mod = class extends mod.Base {
     enable() {
         // Thumbnails on main monitor
         this.bkp_MAX_THUMBNAIL_SCALE = workspaceThumbnail.MAX_THUMBNAIL_SCALE
         workspaceThumbnail.MAX_THUMBNAIL_SCALE = 0.1
-
+        
         // Thumbnails on second monitor
+        if (!SecondaryMonitorDisplay) return; // gnome 42: this object does not exist when there is only one monitor or during login
         this.bkp_SecondaryMonitorDisplay_getThumbnailsHeight = SecondaryMonitorDisplay.prototype._getThumbnailsHeight
         SecondaryMonitorDisplay.prototype._getThumbnailsHeight = function(box) {
-            if (!this._thumbnails.visible)
-                return 0;
-
-            const [width, height] = box.get_size();
-            const { expandFraction } = this._thumbnails;
-            const [thumbnailsHeight] = this._thumbnails.get_preferred_height(width);
-            return Math.min(
-                thumbnailsHeight * expandFraction,
-                height * workspaceThumbnail.MAX_THUMBNAIL_SCALE);
+                if (!this._thumbnails.visible)
+                    return 0;                
+                const [width, height] = box.get_size();
+                const { expandFraction } = this._thumbnails;
+                const [thumbnailsHeight] = this._thumbnails.get_preferred_height(width);
+                return Math.min(
+                    thumbnailsHeight * expandFraction,
+                    height * workspaceThumbnail.MAX_THUMBNAIL_SCALE);
         }
     }
 
@@ -29,8 +29,10 @@ var Mod = class extends mod.Base {
             workspaceThumbnail.MAX_THUMBNAIL_SCALE = this.bkp_MAX_THUMBNAIL_SCALE
         }
 
+        if (!SecondaryMonitorDisplay) return; // gnome 42: this object does not exist when there is only one monitor or during login
         if (this.bkp_SecondaryMonitorDisplay_getThumbnailsHeight) {
             SecondaryMonitorDisplay.prototype._getThumbnailsHeight = this.bkp_SecondaryMonitorDisplay_getThumbnailsHeight
         }
     }
 }
+
